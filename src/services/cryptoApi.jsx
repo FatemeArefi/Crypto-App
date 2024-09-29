8f908c7e71d1c35599860bfe04858470f0e0f68e
Fateme Arefi <f.arefi982@gmail.com>
2024-09-29 21:40:38 +0330

    API kEY Changed

--- src/services/cryptoApi.jsx
+++ src/services/cryptoApi.jsx
@@ -1,7 +1,7 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
-const API_KEY = "CG-43YKxAQCbJTLZVsP9d1NNnLm";
+const API_KEY = "CG-3GC83jHz2vCW8VR2ZZf7wBqp";
 
 const getCoinList = () => {
-  return `${BASE_URL}/coins/markets?vs_currency=usd&ids=bitcoin&per_page=20&page=1&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=2&x_cg_demo_api_key=${API_KEY}`;
 };
 export { getCoinList };
