bc3d64dc6156b54d33d2c8669b3c862cd6a4e79e
FatemeArefi <f.arefi982@gmail.com>
2024-10-02 23:12:04 +0330

    pagination of coins

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -4,21 +4,26 @@ import { useEffect } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
 import Pagination from "../modules/pagination";
+
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, []);
+  }, [page]);
+
   return (
     <div>
-      <Pagination />
+      <Pagination page={page} setPage={setPage} />
       <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
